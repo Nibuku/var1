a4cb936eaa813ce9b4b932793a2df6e51152a7dd
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-06-21 15:41:20 +0400

    header done

--- include/functions.h
+++ include/functions.h
@@ -1,19 +1,21 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
 class Account {
 protected:
 	Account() = default; //поля+дефолтный конструктор
-	string name;
-	double balance;
-	double percent;
-
-public:
 	Account(const Account& rhs) = delete; //правило трех
 	Account& operator=(const Account& rhs) = delete;
+
+	string name;
+	double balance;
+
+public:
+	
 	virtual ~Account()=default;
 
 	string get_name() const; //гетеры и сетеры
@@ -28,6 +30,8 @@ public:
 	virtual void print(ostream& out) const = 0;
 
 };
+
+using AccountPtr = shared_ptr <Account>;
 
 class Settlement : public Account {
 public:
@@ -46,10 +50,13 @@ public:
 };
 
 class Deposit : public Account {
+	double percent;
 public:
 	Deposit();
-	Deposit(string name, double balance);
-	Deposit();
+	Deposit(double percent);
+	~Deposit();
+
+	double get_percent() const;
 
 	virtual unique_ptr<Account> clone() const override;
 	virtual bool equals(shared_ptr<Account>) const override;
@@ -62,10 +69,13 @@ public:
 };
 
 class Credit : public Account {
+	double percent;
 public:
 	Credit();
-	Credit(string name, double balance);
-	Credit();
+	Credit(double percent);
+	~Credit();
+
+	double get_percent() const;
 
 	virtual unique_ptr<Account> clone() const override;
 	virtual bool equals(shared_ptr<Account>) const override;
@@ -80,5 +90,28 @@ public:
 
 class AccountList {
 
-	vector <Account> _account;
+	vector <AccountPtr> _account;
+
+public: 
+
+	AccountList();
+	AccountList(const AccountList& other);
+	AccountList& operator=(const AccountList& other);
+
+	int size();
+	void add(int index, AccountPtr rhs);
+	void remove(int index);
+
+	string max_balance();
+
+	AccountPtr operator[](int index) const;
+	AccountPtr& operator[](int ind);
+	AccountPtr& operator=(const AccountPtr& rhs);
+	friend bool operator==(const AccountPtr& list, const AccountPtr& other);
+	friend bool operator!=(const AccountPtr& list, const AccountPtr& other);
+	AccountList& operator>>(AccountList& rhs);
+
+	void Swap(AccountList& other) noexcept;
+	void show_all();
+	void clear();
 };
